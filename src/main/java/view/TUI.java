f0f1ac39e1c20cb095f1c9cbc1b08d8c6cf10bbe
Joe-Briefcase <s185143@student.dtu.dk>
2019-02-16 15:43:24 +0100

    Interface til TUI implementeret samt tilføjet til TUI klassen.

--- src/main/java/view/TUI.java
+++ src/main/java/view/TUI.java
@@ -2,13 +2,20 @@ package view;
 
 import businesslogic.IUserController;
 
-public class TUI {
+import java.util.Scanner;
+
+public class TUI implements IUI {
 
 
     IUserController userController;
+    private Scanner scanner;
 
     public TUI(IUserController userController) {
         this.userController = userController;
+
+        // Skal nok skrives om så TUI kun tager en scanner som parameter
+        // og ikke initiere den selv (for at undgå at TUI er en creator).
+        scanner = new Scanner(System.in);
     }
 
 
@@ -16,4 +23,41 @@ public class TUI {
 
     }
 
+    public void showMenu() {
+        switch (scanner.nextInt()) {
+            case 1: createUser();
+                break;
+            case 2: listUsers();
+                break;
+            case 3: updateUser();
+                break;
+            case 4: deleteUser();
+                break;
+            case 5: exitProgram();
+                break;
+        }
+    }
+
+    // Følgende metoder vil - når de er implementeret - kalde metoder
+    // i det underliggende lag.
+
+    public void createUser() {
+
+    }
+
+    public void listUsers() {
+
+    }
+
+    public void updateUser() {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+    public void exitProgram() {
+
+    }
 }
