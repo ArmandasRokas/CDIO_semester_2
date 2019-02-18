34b39b719db2ca63e0e36f6c39d6053003fa260c
Joe-Briefcase <s185143@student.dtu.dk>
2019-02-18 10:28:15 +0100

    Tilføjet tekst der beskriver TUI's funktion. createUser() har fået input fra scanner.

--- src/main/java/view/TUI.java
+++ src/main/java/view/TUI.java
@@ -24,6 +24,8 @@ public class TUI implements IUI {
     }
 
     public void showMenu() {
+        System.out.println("1. Create User\n2. List Users\n3. Update User\n4. Delete User\n5. Exit Program");
+
         switch (scanner.nextInt()) {
             case 1: createUser();
                 break;
@@ -42,7 +44,21 @@ public class TUI implements IUI {
     // i det underliggende lag.
 
     public void createUser() {
+        int userID;
+        String userName;
+        String cpr;
 
+        do {
+            userID = scanner.nextInt();
+        }while (userID < 11 || userID > 99);
+
+        do {
+            userName = scanner.nextLine();
+        }while (userName.length() < 2 || userName.length() > 20);
+
+        do {
+            cpr = scanner.nextLine();
+        }while (cpr.length() != 10);
     }
 
     public void listUsers() {
