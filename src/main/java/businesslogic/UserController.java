80b883cfc8fac95f6a262a4924711b1e88e5c5bc
Emil Johannes Lundqvist <emlunde@gmail.com>
2019-02-18 17:23:42 +0100

    updated generateIni() in UserController.

--- src/main/java/businesslogic/UserController.java
+++ src/main/java/businesslogic/UserController.java
@@ -26,7 +26,7 @@ public class UserController implements IUserController {
         // generare pass
 
 
-        UserDTO user = new UserDTO(userId, name, "ganerated-ini", cpr, "generated-pass", roles);
+        UserDTO user = new UserDTO(userId, name, generateIni(name), cpr, "generated-pass", roles);
 
         try {
             userDAO.createUser(user);
@@ -97,17 +97,22 @@ public class UserController implements IUserController {
     }
 
     public String generateIni(String name){
+        name.toUpperCase();
         String[] splitNamesArr = name.split(" ");
         String ini = "";
+        //TODO ryd tomme elementer i splitNamesArr op
+        //Hvis der er "  " mere end et 'space' vil der oprettes tomme elementer, og de skal sorteres fra.
 
         for( int i = 0 ; i < splitNamesArr.length ; i++) {
             ini += splitNamesArr[i].charAt(0);
         }
 
+        //If more than 4 names in total, including first name, last name and middle names,
+        // the initial from first name and last name is generated.
         if(splitNamesArr.length > 4){
-            return splitNamesArr[0] + splitNamesArr[splitNamesArr.length - 1];
-        } else if (splitNamesArr.length < 2){
-            return ini.charAt(0) + ini.charAt(1) + "";
+            return splitNamesArr[0].substring(0,1) + splitNamesArr[splitNamesArr.length - 1].substring(0,1);
+        } else if (splitNamesArr.length < 2){ //if only one name is given, the first to characters is generated as initials.
+            return ini.substring(0,1) + ini.substring(1,2);
         } else {
             return ini;
         }
