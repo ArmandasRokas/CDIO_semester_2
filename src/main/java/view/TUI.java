5dca9ff88a41b064d3f9d6016682c6275809f05c
Casper Bøgeskov Hansen <casphan@ruc.dk>
2019-02-25 13:31:26 +0100

    Reworked TUI to not have duplicate code (baad)

--- src/main/java/view/TUI.java
+++ src/main/java/view/TUI.java
@@ -19,6 +19,12 @@ public class TUI implements IUI {
         scanner = new Scanner(System.in);
     }
 
+    private int userID = 0;
+    private String userName = "";
+    private String cpr = "";
+    private String role = "";
+    private List<String> roles = new ArrayList<String>();
+
     public void showMenu() {
         System.out.println("1. Create User\n2. List Users\n3. Update User\n4. Delete User\n5. Exit Program");
 
@@ -36,57 +42,7 @@ public class TUI implements IUI {
         }
     }
 
-    public void createUser() {
-        int userID;
-        String userName;
-        String cpr;
-        String role;
-        List<String> roles = new ArrayList<String>();
-
-        do {
-            System.out.println("Write id:");
-            userID = scanner.nextInt();
-            if (userID < 11 || userID > 99){
-                System.out.println("Wrong id format");
-            }
-        }while (!userController.isUserIdAvailable(userID));
-
-        do {
-            System.out.println("Write name");
-            userName = scanner.nextLine();
-        }while (userName.length() < 2 || userName.length() > 20);
-
-        do {
-            System.out.println("Write cpr");
-            cpr = scanner.nextLine();
-        }while (cpr.length() != 10);
-
-        while (scanner.hasNext()){
-            role = scanner.nextLine();
-            roles.add(role);
-        }
-
-        try {
-            userController.createUser(userID, userName, cpr, roles);
-        } catch (IUserDAO.DALException e) {
-            System.err.println(e.getMessage());
-            createUser();
-        }
-    }
-
-    public void listUsers() {
-        for(UserDTO user: userController.getUserList()){
-            System.out.println(user.toString());
-        }
-    }
-
-    public void updateUser() {
-        int userID;
-        String userName;
-        String cpr;
-        String role;
-        List<String> roles = new ArrayList<String>();
-
+    public void runConsole(){
         do {
             System.out.println("Enter ID of user to be updated: ");
             userID = scanner.nextInt();
@@ -115,8 +71,29 @@ public class TUI implements IUI {
             role = scanner.nextLine();
             roles.add(role);
         }while (true);
+    }
+
+    public void createUser() {
+        runConsole();
+
+        try {
+            userController.createUser(userID, userName, cpr, roles);
+        } catch (IUserDAO.DALException e) {
+            System.err.println(e.getMessage());
+            createUser();
+        }
+    }
+
+    public void updateUser() {
+        runConsole();
 
         userController.updateUser(userID, userName, cpr, roles);
+    }
+
+    public void listUsers() {
+        for(UserDTO user: userController.getUserList()){
+            System.out.println(user.toString());
+        }
     }
 
     public void deleteUser() {
